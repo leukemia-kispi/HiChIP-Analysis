f5fe7fe960cd677e8e9253cf428812ce7ea53fbc
ValdemarP267 <63105944+ValdemarP267@users.noreply.github.com>
2023-12-28 05:10:17 +0100

    Script Update

--- HiChIP.md
+++ HiChIP.md
@@ -2,7 +2,7 @@
 
 Example presented here are based on data generated with the adapted Dovetail MNase-HiChIP kit. The TCF3::HLF fusion protein was targeted with TCF3 antibody from Cell signaling in the HAL-01 TCF3::HLF positive leukemia cell line CRISPR engineered to knockout wild type TCF3 expression and interfere with fusion protein targeted pulldown.
 
-## Install python3 and pip3.
+## Install python3, pip3 and java.
 
 These are required, if you don’t already have them installed, you will need sudo privileges.
 
@@ -11,6 +11,7 @@ Update and install python3 and pip3:
 ```
 sudo apt-get update
 sudo apt-get install python3 python3-pip
+sudo apt install default-jre
 ```
 
 To set python3 and pip3 as primary alternative to avoid version conflicts:
@@ -25,17 +26,21 @@ Enter the generated DovetailHiChIP Conda environment after running DirectoryArch
 ```
 conda activate DovetailHiChIP
 ```
+## Install remaining dependecies
 
-Clone repository from dovetail-genomics:
+Clone repository from dovetail-genomics and  pull juicertools.jar(moved into dovetails-genomics HiChIP directory):
 
 ```
 git clone https://github.com/dovetail-genomics/HiChiP.git
+wget https://s3.amazonaws.com/hicfiles.tc4ga.com/public/juicer/juicer_tools_1.22.01.jar
+mv juicer_tools_1.22.01.jar ./HiChiP/juicertools.jar
 ```
 
-Make enrichment_stats.sh script executable:
+Make enrichment_stats.sh and installDep.sh script executable:
 
 ```
 chmod +x ./HiChiP/enrichment_stats.sh
+chmod +x ./HiChiP/installDep.sh
 ```
 
 Use the installDep.sh script from repository to ensure all dependecies are installed in the conda environment. In addition to installing GCC make, python and pip in case they are missing, it will include following dependencies:
@@ -54,10 +59,7 @@ Use the installDep.sh script from repository to ensure all dependecies are insta
 - py2bit 
 - pyBigWig 
 
-Set permissions to file and run the installation script.
-
 ```
-chmod +x ./HiChiP/installDep.sh
 ./HiChiP/installDep.sh
 ```
 
@@ -92,12 +94,32 @@ No need to specify an output path, the bwa index files are automatically generat
 To avoid memory issues, some of the steps require writing temporary files into a temp folder, please generate a temp folder and remember its full path. Temp files may take up to x3 of the space that the fastq.gz files are taking, that is, if the total volume of the fastq files is 5Gb, make sure that the temp folder can store at least 15Gb.
 
 ```
-mkdir <full_path/to/tmpdir>
+mkdir /mnt/tmp
 ```
 
 ## Run the TCF3_HiChIP_fusedRep.sh script
 
-The script will trim adapters and short reads in a Trim_Galore conda environment and followed by initiating a DovetailHiChIP environment. Trimmed replicates will be fused 
+The script will:
+- trim adapters and short reads in a Trim_Galore conda environment
+- Initiate DovetailHiChIP conda environment and fuse Trimmed replicate reads for alignment
+- QC stats and plot by comparing to provided TCF3::HLF ChIPseq
+- Generate bigwig fiels for IGV browsing
+- call 1D peaks with MACS2 in MACS2 conda environment
 
 **Good Practice**
 It is also recommended to run the script TCF3_HiChIP_singleRep.sh. This one omits the fuse step and aligns each replicate seperatly. Comparing these output to the fused data outputs can ensure higher confidence in the called interactions from running FitHiChIP.
+
+#FitHiChIP Loop calling
+conda activate /home/ubuntu/My_HiCPro_ENV/
+
+export PATH="/home/ubuntu/My_HiC-Pro/HiC-Pro_3.0.0/bin/":$PATH
+
+#FitHiChIP Loop Calling
+#Filter pairs
+pairtools select '(pair_type=="UU") or (pair_type=="UR") or (pair_type=="RU") or (pair_type=="uu") or (pair_type=="Uu")  or (pair_type=="uU")' JoinedRep_TCF3_HLF_hg38_nodd_hicpro_mapped.pairs -o JoinedRep_TCF3-HLF_hg38_nodd_mapped.filtered.pairs
+
+#HiCPro Valid Pairs Files
+grep -v '#' JoinedRep_TCF3-HLF_hg38_nodd_mapped.filtered.pairs| awk -F"\t" '{print $1"\t"$2"\t"$3"\t"$6"\t"$4"\t"$5"\t"$7}' | gzip -c > JoinedRep_TCF3-HLF_hg38_nodd_hicpro_mapped.filtered.pairs.gz
+
+cd /home/ubuntu/My_HiC-Pro/FitHiChIP/
+bash ./FitHiChIP_Docker.sh -C /home/ubuntu/My_HiC-Pro/FitHiChIP/configfile_CB_TCF3_JoinedRep_5kb_Merge_50kb_3M
--- TCF3_HiChIP_fusedRep.sh
+++ TCF3_HiChIP_fusedRep.sh
@@ -6,10 +6,13 @@ REF_GENOME="/mnt/0.GenomeAssembly/GRCh38_no_alt_ref.genome"
 BLACKLIST="/mnt/0.BlackList/hg38-blacklist.v2.bed "
 # Set Path for read *fg.gz files
 FASTQ_DIR="/mnt/1.RawData"
+HiChIP_R1="/mnt/3.TRIM/JoinedFastq_R1.fq.gz"
+HiChIP_R2="/mnt/3.TRIM/JoinedFastq_R1.fq.gz"
 # Set output directories
 OUTPUT_DIR_TRIM="/mnt/3.TRIM"
 OUTPUT_HICHIP_ALIGN="/mnt/4.HiChIP_Alignment"
-OUTPUT_HICHIP_SUB="/mnt/4.HiChIP_Alignment/"
+OUTPUT_HICHIP_SUB="/mnt/4.HiChIP_Alignment/Outputs"
+OUTPUT_MACS2="/mnt/5.MACS2"
 # Thread usage
 cores= 32
 #Thread usage for pairtools dedup and split processes
@@ -59,12 +62,11 @@ cat "$OUTPUT_DIR_TRIM"/*_R2_val_2.fq.gz > JoinedFastq_R2.fq.gz
 
 # Alignment
 cd $OUTPUT_HICHIP_ALIGN
-HiChiP_R1="/mnt/3.TRIM/JoinedFastq_R1.fq.gz"
-HiChiP_R2="/mnt/3.TRIM/JoinedFastq_R1.fq.gz"
 MAPPED_PAIRS="JoinedRep_TCF3_HLF_hg38_nodd_mapped.pairs"
 MAPPED_BAM="JoinedRep_TCF3_HLF_hg38_nodd_mapped.PT.bam"
 
-bwa mem -5SP -T0 -t$cores $REF_FASTA $HiChiP_R1 $HiChiP_R2 | \
+#pairtools dedup step omited
+bwa mem -5SP -T0 -t$cores $REF_FASTA $HiChIP_R1 $HiChIP_R2 | \
 pairtools parse --min-mapq 40 --walks-policy 5unique --max-inter-align-gap 30 --nproc-in $cores2 --nproc-out $cores2 --chroms-path $REF_GENOME | \
 pairtools sort --tmpdir=$TEMP--nproc $cores|\
 #pairtools dedup --nproc-in $cores2 --nproc-out $cores2 --mark-dups --output-stats <stats.txt>|\
@@ -75,32 +77,31 @@ samtools sort -@$cores -o $MAPPED_BAM;samtools index $MAPPED_BAM
 echo "HiCHIP Aligmnent Complete"
 
 #QC compare ChIP-seq TCF3-HLF_FLAG
-bash enrichment_stats.sh -g /mnt/GRCh38_assembly/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -b /mnt/Dovetail_Pipeline/TCF3-HLF_HAL01_hg38_JoinedRep/JoinedRep_TCF3_HLF_hg38_nodd_mapped.PT.bam -p /mnt/ChIP-Seq/IDR_cle/Oracle2_HAL-01_TCF3-HLF_FLAG_cle-idr.bed -t 16 -x HiChIP-TCF3-HLF
-bash enrichment_stats.sh -g /mnt/GRCh38_assembly/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -b /mnt/Dovetail_Pipeline/TCF3-HLF_HAL01_hg38_JoinedRep/JoinedRep_TCF3_HLF_hg38_nodd_mapped.PT.bam -p /mnt/ChIP-Seq/IDR_cle/Oracle2_HAL-01_TCF3-HLF_FLAG_bw175_cle-idr.bed -t 16 -x HiChIP-TCF3-HLF_bw175
+bash ./HiChIP/enrichment_stats.sh -g $REF_FASTA -b $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM  -p ./HiChIP_Analysis/ChIP-Seq/Oracle2_HAL-01_TCF3-HLF_FLAG_bw175_cle-idr.bed -t $cores2 -x $OUTPUT_HICHIP_SUB/HiChIP-TCF3-HLF_bw175
+
 
 #QC Plot ChIP-seq TCF3-HLF_FLAG
-python3 plot_chip_enrichment_bed.py -bam /mnt/Dovetail_Pipeline/TCF3-HLF_HAL01_hg38_JoinedRep/JoinedRep_TCF3_HLF_hg38_nodd_mapped.PT.bam -peaks /mnt/ChIP-Seq/IDR_cle/Oracle2_HAL-01_TCF3-HLF_FLAG_cle-idr.bed -output HiChIP_TCF3_HLF_ChIP_FLAG_enrichment.png
-python3 plot_chip_enrichment_bed.py -bam /mnt/Dovetail_Pipeline/TCF3-HLF_HAL01_hg38_JoinedRep/JoinedRep_TCF3_HLF_hg38_nodd_mapped.PT.bam -peaks /mnt/ChIP-Seq/IDR_cle/Oracle2_HAL-01_TCF3-HLF_FLAG_bw175_cle-idr.bed -output HiChIP_TCF3_HLF_ChIP_FLAG_bw175_enrichment.png
+python3 ./HiChIP/plot_chip_enrichment_bed.py -bam $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM -peaks ./HiChIP_Analysis/ChIP-Seq/Oracle2_HAL-01_TCF3-HLF_FLAG_bw175_cle-idr.bed -output $OUTPUT_HICHIP_SUB/HiChIP_TCF3_HLF_ChIP_FLAG_bw175_enrichment.png
+
+echo "HiCHIP Aligmnent QC Complete"
 
 #Enrichment for IGV
 conda activate deeptool
-bamCoverage -b JoinedRep_TCF3_HLF_hg38_nodd_mapped.PT.bam -o BLF_JoinedRep_TCF3_HLF_hg38_nodd_mapped.bw --effectiveGenomeSize 2913022398 -bl $BLACKLIST --normalizeUsing RPKM -p max -bs 10 --extendReads --ignoreForNormalization M
+bamCoverage -b $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM -o $OUTPUT_HICHIP_SUB/BLF_JoinedRep_TCF3_HLF_hg38_nodd_mapped.bw --effectiveGenomeSize 2913022398 -bl $BLACKLIST --normalizeUsing RPKM -p max -bs 10 --extendReads --ignoreForNormalization M
 
 #ContacMaps
-conda activate Juicebox
-java -Xmx48000m  -Djava.awt.headless=true -jar /home/ubuntu/My_HiC-Pro/juicer_tools_1.22.01.jar pre --threads 32 JoinedRep_TCF3_HLF_hg38_nodd_mapped.pairs JoinedRep_TCF3-HLF_HAL01_hg38_nodd_contact_map.hic /mnt/GRCh38_assembly/GRCh38_no_alt_ref.genome
+java -Xmx48000m  -Djava.awt.headless=true -jar ./HiChIP/juicer_tools_1.22.01.jar pre --threads $cores $OUTPUT_HICHIP_ALIGN/JoinedRep_TCF3_HLF_hg38_nodd_mapped.pairs $OUTPUT_HICHIP_SUB/JoinedRep_TCF3-HLF_HAL01_hg38_nodd_contact_map.hic $REF_GENOME
 
 #call 1D peaks with MACS2
 conda activate MACS2
 
 #Remove black list
-bedtools intersect -v -abam JoinedRep_TCF3_HLF_hg38_nodd_mapped.PT.bam -b /mnt/BlastListPeaks/hg38-blacklist.v2.bed > BLF_JoinedRep_TCF3-HLF_HAL01_hg38_nodd_mapped.PT.bam
+bedtools intersect -v -abam $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM -b $BLACKLIST > $OUTPUT_HICHIP_ALIGN/BLF_JoinedRep_TCF3-HLF_HAL01_hg38_nodd_mapped.PT.bam
 
 #MACS2 Peak calling on BlackList filtered BAM.
-samtools view -b BLF_JoinedRep_TCF3_HLF_hg38_nodd_mapped.PT.bam -h -F 0x900 | bedtools bamtobed -i stdin > BLF_JoinedRep_TCF3_HLF_hg38_nodd_primary.aln.bed
+samtools view -b $OUTPUT_HICHIP_ALIGN/BLF_JoinedRep_TCF3_HLF_hg38_nodd_mapped.PT.bam -h -F 0x900 | bedtools bamtobed -i stdin > $OUTPUT_HICHIP_ALIGN/BLF_JoinedRep_TCF3_HLF_hg38_nodd_primary.aln.bed
 
-macs2 callpeak -t BLF_JoinedRep_TCF3_HLF_hg38_nodd_primary.aln.bed -p 0.000000001 -n BLF_JoinedRep_TCF3_HLF_hg38_nodd_p9.macs2
-macs2 callpeak -t BLF_JoinedRep_TCF3-HLF_HAL01_hg38_nodd_primary.aln.bed -p 0.000000001 -g 2913022398 -n BLF_JoinedRep_TCF3_HLF_gs_hg38_nodd_p9.macs2
+macs2 callpeak -t $OUTPUT_HICHIP_ALIGN/BLF_JoinedRep_TCF3_HLF_hg38_nodd_primary.aln.bed -p 0.000000001 -g 2913022398 -n $OUTPUT_HICHIP_SUB/BLF_JoinedRep_TCF3_HLF_gs_hg38_nodd_p9.macs2
 
 #Oracle File for IDR and IDR
 macs2 callpeak -t BLF_JoinedRep_TCF3-HLF_HAL01_hg38_nodd_primary.aln.bed --keep-dup 10 --min-length 300 -p 0.000000001 --bw 300 --mfold 5 50 -n BLF_JoinedRep_TCF3_HLF_hg38_nodd_kdp9Oracle.macs2
@@ -115,7 +116,6 @@ conda activate /home/ubuntu/My_HiCPro_ENV/
 
 export PATH="/home/ubuntu/My_HiC-Pro/HiC-Pro_3.0.0/bin/":$PATH
 
-BLF_TCF3-HLF_HAL01_rep1_hg38_nodd_kdp5bw300ml300Sort.macs2_peaks.narrowPeak
 #FitHiChIP Loop Calling
 #Filter pairs
 pairtools select '(pair_type=="UU") or (pair_type=="UR") or (pair_type=="RU") or (pair_type=="uu") or (pair_type=="Uu")  or (pair_type=="uU")' JoinedRep_TCF3_HLF_hg38_nodd_hicpro_mapped.pairs -o JoinedRep_TCF3-HLF_hg38_nodd_mapped.filtered.pairs
