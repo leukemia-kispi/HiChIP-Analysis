372dc7ab18b78a127891a00a9772763d31718a56
ValdemarP267 <63105944+ValdemarP267@users.noreply.github.com>
2024-01-04 03:52:17 +0100

    singlerep update

--- TCF3_HiChIP_singleRep.sh
+++ TCF3_HiChIP_singleRep.sh
@@ -74,25 +74,26 @@ for num in "${NUMBERS[@]}"; do
     pairtools split --nproc-in $cores2 --nproc-out $cores2 --output-pairs $MAPPED_PAIRS --output-sam -|\
     samtools view -bS -@$cores | \
     samtools sort -@$cores -o $MAPPED_BAM;samtools index $MAPPED_BAM
+
+    echo "HiCHIP Aligmnent Complete for rep${num}"
+
+    cd /home/ubuntu
+
+    #QC compare ChIP-seq TCF3-HLF_FLAG
+    bash ./HiChIP/enrichment_stats.sh -g $REF_FASTA -b $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM  -p ./HiChIP_Analysis/ChIP-Seq/Oracle2_HAL-01_TCF3-HLF_FLAG_bw175_cle-idr.bed -t $cores2 -x $OUTPUT_HICHIP_SUB/HiChIP_rep${num}_TCF3HLF_bw175
+
+    #QC Plot ChIP-seq TCF3-HLF_FLAG
+    python3 ./HiChIP/plot_chip_enrichment_bed.py -bam $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM -peaks ./HiChIP_Analysis/ChIP-Seq/Oracle2_HAL-01_TCF3-HLF_FLAG_bw175_cle-idr.bed -output $OUTPUT_HICHIP_SUB/HiChIP_rep${num}_TCF3HLF_ChIP_FLAG_bw175_enrichment.png
+
+    echo "HiCHIP Aligmnent QC Complete for rep${num}"
+
+    #Enrichment for IGV
+    bamCoverage -b $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM -o $OUTPUT_HICHIP_SUB/BLF_rep${num}_TCF3HLF_hg38_nodd_mapped.bw --effectiveGenomeSize 2913022398 -bl $BLACKLIST --normalizeUsing RPKM -p max -bs 10 --extendReads --ignoreForNormalization M
+
+    echo "Generated Bigwig file Complete for rep${num}"
+
+    #ContacMaps
+    java -Xmx48000m  -Djava.awt.headless=true -jar /home/ubuntu/HiChIP/juicer_tools_1.22.01.jar pre --threads $cores $OUTPUT_HICHIP_ALIGN/JoinedRep_TCF3_HLF_hg38_nodd_mapped.pairs $OUTPUT_HICHIP_SUB/rep${num}_TCF3HLF_HAL01_hg38_nodd_contact_map.hic $REF_GENOME
+
+    echo "Generated .hic file Complete rep${num}"
 done
-echo "HiCHIP Aligmnent Complete"
-
-cd /home/ubuntu
-
-#QC compare ChIP-seq TCF3-HLF_FLAG
-bash ./HiChIP/enrichment_stats.sh -g $REF_FASTA -b $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM  -p ./HiChIP_Analysis/ChIP-Seq/Oracle2_HAL-01_TCF3-HLF_FLAG_bw175_cle-idr.bed -t $cores2 -x $OUTPUT_HICHIP_SUB/HiChIP-TCF3-HLF_bw175
-
-#QC Plot ChIP-seq TCF3-HLF_FLAG
-python3 ./HiChIP/plot_chip_enrichment_bed.py -bam $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM -peaks ./HiChIP_Analysis/ChIP-Seq/Oracle2_HAL-01_TCF3-HLF_FLAG_bw175_cle-idr.bed -output $OUTPUT_HICHIP_SUB/HiChIP_TCF3_HLF_ChIP_FLAG_bw175_enrichment.png
-
-echo "HiCHIP Aligmnent QC Complete"
-
-#Enrichment for IGV
-bamCoverage -b $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM -o $OUTPUT_HICHIP_SUB/BLF_JoinedRep_TCF3_HLF_hg38_nodd_mapped.bw --effectiveGenomeSize 2913022398 -bl $BLACKLIST --normalizeUsing RPKM -p max -bs 10 --extendReads --ignoreForNormalization M
-
-echo "Generated Bigwig file Complete"
-
-#ContacMaps
-java -Xmx48000m  -Djava.awt.headless=true -jar /home/ubuntu/HiChIP/juicer_tools_1.22.01.jar pre --threads $cores $OUTPUT_HICHIP_ALIGN/JoinedRep_TCF3_HLF_hg38_nodd_mapped.pairs $OUTPUT_HICHIP_SUB/JoinedRep_TCF3-HLF_HAL01_hg38_nodd_contact_map.hic $REF_GENOME
-
-echo "Generated .hic file Complete"
