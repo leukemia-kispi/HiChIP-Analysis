a318373b199f5215f81a6278fd7d09d16245f0b2
ValdemarP267 <63105944+ValdemarP267@users.noreply.github.com>
2024-01-07 17:24:43 +0100

    MACS2 update

--- Scripts/MACS2.sh
+++ Scripts/MACS2.sh
@@ -25,6 +25,8 @@ PRIMARY_ALN_Rep2="BLF_Rep2_TCF3_HLF_hg38_nodd_primary.aln.bed"
 MACS2_JoinedRep="BLF_JoinedRep_TCF3_HLF_gs_hg38_nodd_p9.macs2"
 MACS2_JoinedRep_Oracle="BLF_JoinedRep_TCF3_HLF_Oracle.macs2"
 MACS2_JoinedRep_SORT="BLF_JoinedRep_TCF3_HLF_OracleSort.macs2"
+$MACS2_Rep1="BLF_Rep1_TCF3-HLF_gs_hg38_p9.macs2"
+$MACS2_Rep2="BLF_Rep2_TCF3-HLF_gs_hg38_p9.macs2"
 MACS2_Rep1_Permissive="BLF_Rep1_TCF3_HLF_Permissive.macs2"
 MACS2_Rep2_Permissive="BLF_Rep2_TCF3_HLF_Permissive.macs2"
 MACS2_Rep1_SORT="BLF_Rep1_TCF3_HLF_PermissiveSort.macs2"
@@ -43,8 +45,8 @@ fi
 
 #Remove black list.
 #bedtools intersect -v -abam $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM -b $BLACKLIST > $OUTPUT_HICHIP_ALIGN/$MAPPED_BLF_BAM
-bedtools intersect -v -abam $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM_Rep1 -b $BLACKLIST > $OUTPUT_HICHIP_ALIGN/$MAPPED_BLF_BAM_Rep1
-bedtools intersect -v -abam $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM_Rep2 -b $BLACKLIST > $OUTPUT_HICHIP_ALIGN/$MAPPED_BLF_BAM_Rep2
+#bedtools intersect -v -abam $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM_Rep1 -b $BLACKLIST > $OUTPUT_HICHIP_ALIGN/$MAPPED_BLF_BAM_Rep1
+#bedtools intersect -v -abam $OUTPUT_HICHIP_ALIGN/$MAPPED_BAM_Rep2 -b $BLACKLIST > $OUTPUT_HICHIP_ALIGN/$MAPPED_BLF_BAM_Rep2
 
 echo "Black list regions removed"
 
@@ -55,9 +57,9 @@ samtools view -b $OUTPUT_HICHIP_ALIGN/$MAPPED_BLF_BAM_Rep2 -h -F 0x900 | bedtool
 
 echo "Primary alignment .bed file generated"
 
-macs2 callpeak -t $OUTPUT_HICHIP_ALIGN/$PRIMARY_ALN -p 0.000000001 -g 2913022398 -n $OUTPUT_MACS2/$MACS2_JoinedRep
-macs2 callpeak -t $OUTPUT_HICHIP_ALIGN/$PRIMARY_ALN_Rep1 -p 0.000000001 -g 2913022398 -n $OUTPUT_MACS2/$MACS2_Rep1
-macs2 callpeak -t $OUTPUT_HICHIP_ALIGN/$PRIMARY_ALN_Rep2 -p 0.000000001 -g 2913022398 -n $OUTPUT_MACS2/$MACS2_Rep2
+macs2 callpeak -t $OUTPUT_HICHIP_SUB/$PRIMARY_ALN -p 0.000000001 -g 2913022398 -n $OUTPUT_MACS2/$MACS2_JoinedRep
+macs2 callpeak -t $OUTPUT_HICHIP_SUB/$PRIMARY_ALN_Rep1 -p 0.000000001 -g 2913022398 -n $OUTPUT_MACS2/$MACS2_Rep1
+macs2 callpeak -t $OUTPUT_HICHIP_SUB/$PRIMARY_ALN_Rep2 -p 0.000000001 -g 2913022398 -n $OUTPUT_MACS2/$MACS2_Rep2
 
 echo "Standard MACS2 peak calling done"
 
@@ -67,6 +69,7 @@ macs2 callpeak -t $OUTPUT_HICHIP_SUB/$PRIMARY_ALN_Rep1 --keep-dup 10 --min-lengt
 macs2 callpeak -t $OUTPUT_HICHIP_SUB/$PRIMARY_ALN_Rep2 --keep-dup 10 --min-length 300 -p 0.000000001 -g 2913022398 --bw 300 --mfold 5 50 -n $OUTPUT_MACS2/$MACS2_Rep2_OracleSet
 macs2 callpeak -t $OUTPUT_HICHIP_SUB/$PRIMARY_ALN_Rep1 --keep-dup 10 --min-length 300 -p 0.00001 -g 2913022398 --bw 300 --mfold 5 50 -n $OUTPUT_MACS2_Permissive/$MACS2_Rep1_Permissive
 macs2 callpeak -t $OUTPUT_HICHIP_SUB/$PRIMARY_ALN_Rep2 --keep-dup 10 --min-length 300 -p 0.00001 -g 2913022398 --bw 300 --mfold 5 50 -n $OUTPUT_MACS2_Permissive/$MACS2_Rep2_Permissive
+sudo chmod 777 -R $OUTPUT_MACS2
 sort -k8,8nr  $OUTPUT_MACS2/$MACS2_JoinedRep_Oracle > $OUTPUT_MACS2_SORT/$MACS2_JoinedRep_SORT
 sort -k8,8nr  $OUTPUT_MACS2_Permissive/$MACS2_Rep1_Permissive > $OUTPUT_MACS2_SORT/$MACS2_Rep1_SORT
 sort -k8,8nr  $OUTPUT_MACS2_Permissive/$MACS2_Rep2_Permissive > $OUTPUT_MACS2_SORT/$MACS2_Rep2_SORT
